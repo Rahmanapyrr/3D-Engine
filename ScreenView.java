74a50e4f9e75923aab38a63fb6d98b402f9e7873
rahmanapyrr <rahmana.muhammad@bison.howard.edu>
2020-10-02 13:22:36 -0400

    Adding the panel view

--- ScreenView.java
+++ ScreenView.java
@@ -2,4 +2,12 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ScreenView {
+    public static void main(String[] args){
+        JFrame frame = new JFrame();
+        Container pane = frame.getContentPanel();
+        pane.setLayout(new BorderLayout());
+    }
+    
+    frame.setSize(800, 1020);
+    frame.setVisible(true);
 }
